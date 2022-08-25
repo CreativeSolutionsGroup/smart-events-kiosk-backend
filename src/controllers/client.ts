26354322a906e1032fff4482505afac989caed38
Konolo <wjacobs@cedarville.edu>
2022-08-25 15:04:08 -0400

    Added the route for updating client

--- src/controllers/client.ts
+++ src/controllers/client.ts
@@ -33,3 +33,36 @@ export const read_all_clients: Express.RequestHandler = async (req, res) => {
 
     res.json(clients);
 }
+
+export const update_one_client: Express.RequestHandler = async (req, res) => {
+    const client: Client = req.body;
+    const id = req.params.id;
+
+
+    const sheet = google.sheets("v4");
+    const read_result = await sheet.spreadsheets.values.get({
+        spreadsheetId: process.env.SHEET_ID,
+        auth: sheet_auth(),
+        range: `${CLIENT_SHEET_ID}!A1:E`
+    });
+
+    const rows = read_result.data.values as string[][];
+    const ser = serialize_rows(rows) as Array<Client>;
+
+    const clientIndex = ser.findIndex((r) => id === r.mac_address);
+    const sheetIndex = clientIndex + 2;
+
+    const row = [...Object.values(client)];
+
+    const request = {
+        spreadsheetId: process.env.SHEET_ID,
+        range: "Client!A" + sheetIndex + ":E" + sheetIndex,
+        valueInputOption: "RAW",
+        auth: sheet_auth(),
+        resource: {
+            values: [row]
+        }
+    }
+
+    sheet.spreadsheets.values.update(request);
+}
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { create_check_in, create_event, read_all_events } from "./controllers/checkins";
-import { read_all_clients, read_one_client } from "./controllers/client";
+import { read_all_clients, read_one_client, update_one_client } from "./controllers/client";
 
 var app = express()
 app.use(cors())
@@ -27,6 +27,7 @@ v1.route("/client")
 
 v1.route("/client/:id")
   .get(read_one_client)
+  .put(update_one_client)
 
 app.use("/v1", v1);
 

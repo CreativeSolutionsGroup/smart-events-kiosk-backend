bd34e46fc7b4cb34d58812aa995669dd3a518c9a
Icedragon10532 <sebastianneiswanger@gmail.com>
2022-09-15 09:24:12 -0400

    Added DELETE route and covered crashes

--- src/controllers/client.ts
+++ src/controllers/client.ts
@@ -98,3 +98,40 @@ export const update_one_client: Express.RequestHandler = async (req, res) => {
 
     res.json(result)
 }
+
+export const remove_one_client: Express.RequestHandler = async (req, res) => {
+    const client: Client = req.body;
+    const id = req.params.id;
+
+    const sheet = google.sheets("v4");
+    const read_result = await sheet.spreadsheets.values.get({
+        spreadsheetId: process.env.SHEET_ID,
+        auth: sheet_auth(),
+        range: `${CLIENT_SHEET_ID}!A1:E`
+    });
+
+    const rows = read_result.data.values as string[][];
+    const ser = serialize_rows(rows) as Array<Client>;
+
+    const clientIndex = ser.findIndex((r) => id === r.mac_address);
+    if (clientIndex === -1) { 
+        res.status(404).end() 
+    } else {
+        const sheetIndex = clientIndex + 2;
+        const emptyRow = ["","","","",""]
+    
+        const request = {
+            spreadsheetId: process.env.SHEET_ID,
+            range: "Client!A" + sheetIndex + ":E" + sheetIndex,
+            valueInputOption: "RAW",
+            auth: sheet_auth(),
+            resource: {
+                values: [emptyRow]
+            }
+        }
+    
+        const result = sheet.spreadsheets.values.update(request);
+    
+        res.json(result)
+    }
+}
